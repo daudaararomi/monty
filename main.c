90e87eaa551ee45f0b7b167754b1642a987388dc
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-12-14 12:15:42 -0800

    push_pall.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+ * main - gets argv[1] file and passes it to stack_it
+ *
+ * @argv: argument vector
+ * @argc: argument count
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	stack_it(argv[1]);
+	return (0);
+}
