7db94405535c9c457dcb8d8442dfd5ebfe98f975
Dauda Olurotimi Araromi <daudaararomi@gmail.com>
2022-12-17 18:10:17 -0800

    1-opcode.c

--- 1-opcode.c
+++ 1-opcode.c
@@ -72,3 +72,28 @@ void free_stack(stack_t **stack)
 		*stack = temp;
 	}
 }
+
+/**
+ * nop - does literally nothing
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ * Return: Nothing.
+ */
+void nop(stack_t **stack, unsigned int nl)
+{
+	(void)stack;
+	(void)nl;
+}
+
+/**
+ * _isalpha - checks if int is in alphabet
+ * @c: int
+ * Return: 1 if yes, 0 if no
+ */
+int _isalpha(int c)
+{
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+		return (1);
+	else
+		return (0);
+}
